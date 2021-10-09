ce9a2aa63f2b2bef6da17d095142153e9e7308b3
Manueluz <manueldelauz@gmail.com>
2021-10-09 03:20:37 +0200

    added logs comments

--- src/Logs/SimpleLogger.java
+++ src/Logs/SimpleLogger.java
@@ -4,7 +4,20 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+
+/**
+ * Simple logger cause i was too lazy to write System.out.println()
+ * + adds some neat features like time
+ * @author Manueluz
+ * @version 1.0
+ */
 public class SimpleLogger {
+
+    
+    /**
+     * Logs a message to the console with the time it was logged
+     * @param log The message to log
+     */
     public static void log(String log){
         DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         Calendar cal = Calendar.getInstance();
