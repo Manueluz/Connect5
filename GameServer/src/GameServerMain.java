b53690e64d5bc868aa5ab62fc71bc553ed15e40c
Manueluz <manueldelauz@gmail.com>
2021-10-12 23:37:14 +0200

    Added logger to server startup

--- GameServer/src/GameServerMain.java
+++ GameServer/src/GameServerMain.java
@@ -1,3 +1,4 @@
+import Logs.SimpleLogger;
 import Managers.GameManager;
 import Networking.Listener;
 
@@ -6,14 +7,17 @@ import java.io.IOException;
 
 public class GameServerMain {
     public static void main(String[] args) throws IOException {
-
+        SimpleLogger.log("[Main]Starting the websocket");
         Linker linker = new Linker();
         linker.start(9090);
-
+        SimpleLogger.log("[Main]Websocket started!");
+        SimpleLogger.log("[Main]Starting the CatHttp server");
         CatHttp httpServer = new CatHttp(9092);
         httpServer.startCatServer();
-
+        SimpleLogger.log("[Main]CatHttp server started!");
+        SimpleLogger.log("[Main]Starting the game server");
         GameManager manager = new GameManager();
         Listener listener = new Listener(9091,manager);
+        SimpleLogger.log("[Main]Game server Started!");
     }
 }
